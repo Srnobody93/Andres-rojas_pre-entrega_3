ec055c51d1c1b70f3afa81957f308fc75dfdfab9
Andres Rojas <andresrojas1999@gmail.com>
2023-03-18 13:28:39 -0500

    statics cargados

--- prentrega3/menu/admin.py
+++ prentrega3/menu/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from menu.models import Compras
 
-# Register your models here.
+admin.site.register(Compras)
--- prentrega3/menu/models.py
+++ prentrega3/menu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Compras(models.Model):
+    nombre = models.CharField(max_length=25)
+    codigo = models.IntegerField()
+    precio = models.FloatField()
+
+    def __str__(self):
+        return f"{self.id} - {self.nombre} - {self.codigo}" 
+    
